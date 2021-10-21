0d40ce2799b1c83423d67a82ffd0c8e66151245a
Adi Skop <adiskop@gmail.com>
2021-10-21 13:56:25 +0200

    change bg picture opacity

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,7 @@ import image from "../swiss.jpg";
 export default function Home(){
     return (
         <main>
-            <img src={image} alt="background picture" className="absolute object-cover w-full h-full"/>
+            <img src={image} alt="background picture" className="absolute object-cover w-full h-full opacity-80"/>
             <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
                 <h1 className="text-6xl text-green-100 font-bold cursive leading-none lg:leading-snug home-name">Hi. I'm Adi.</h1>
             </section>
